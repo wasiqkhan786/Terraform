a2f69b98b23d6edfa14df3fd5a09cc378ecfa433
wasiqkhan786 <khanwasiq616@gmail.com>
2023-09-09 14:01:49 +0200

    initial commit

--- modules/aws_provider_module.tf
+++ modules/aws_provider_module.tf
@@ -1,6 +1,8 @@
 provider "aws" {
-  alias  = "aws_provider"
-  region = "eu-central-1"
+  //alias  = "aws_provider"
+  //region = "eu-central-1"
   access_key = "AKIAYTQCMDFVC763JZ67"
   secret_key = "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"
+
+
  }
--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -1,5 +1,5 @@
   resource "aws_s3_bucket_acl" "S3Bucket" {
     bucket = var.S3bucket
     acl    = "private"
-    # Other S3 bucket settings
+
   }
